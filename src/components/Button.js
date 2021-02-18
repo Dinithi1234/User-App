5591ac03bf7bce988f04548044c8755184b816b6
Dinithi Fernando <30996342+Dinithi1234@users.noreply.github.com>
2021-02-18 21:43:47 +0530

    final commit re added button component

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button({ color, text, onClick }) {
+    return (
+        <button style={{backgroundColor: color}} className='btn' onClick={onClick}>{text}</button>
+    )
+}
+
+export default Button
